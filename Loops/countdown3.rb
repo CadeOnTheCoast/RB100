28b679c7d1cf3890ed3ba48991518a73c8fa89a0
CadeOnTheCoast <cade.kistler@gmail.com>
2020-08-07 08:51:29 -0500

    Adding several exercises from the Loops chapter

---
+++ Loops/countdown3.rb
@@ -0,0 +1,8 @@
+x = gets.chomp.to_i
+
+for i in 1..x do #1..x is an inclusive range in Ruby (...) would indicate non inclusive
+  
+  puts i
+end
+
+puts "Done"
---
+++ Loops/useful_loop.rb
@@ -0,0 +1,8 @@
+# useful_loop.#!/usr/bin/env ruby -wKU
+
+i = 0
+loop do
+  i +=1
+  puts i
+  break
+end
